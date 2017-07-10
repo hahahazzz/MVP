89688dd4541873dadfb5048cd883e3d15b15838c
guang <1607868475@qq.com>
2017-07-10 10:08:08 +0800

    删除ApiService里的测试方法

--- app/src/main/java/com/eflashloan/wct/http/ApiService.java
+++ app/src/main/java/com/eflashloan/wct/http/ApiService.java
@@ -1,17 +1,9 @@
 package com.eflashloan.wct.http;
 
-import android.telecom.Call;
-
-import okhttp3.ResponseBody;
-import retrofit2.http.GET;
-
 /**
  * @Author : QiuGang
  * @Email : 1607868475@qq.com
  * @Date : 2017/7/6 15:29
  */
 public interface ApiService {
-
-    @GET
-    Call<ResponseBody> getUserNameList();
 }
